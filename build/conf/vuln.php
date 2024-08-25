27c2d815decff147cbcfee2e5fe6ea1c18b80416
tembalii <159453738+tembalii@users.noreply.github.com>
2024-08-25 12:08:14 +0100

    Create vuln.php

---
+++ build/conf/vuln.php
@@ -0,0 +1,14 @@
+<?php
+
+//ruleid: search-active-debug
+ini_set("display_errors",1);
+//ruleid: search-active-debug
+ini_set("display_errors",true);
+//ruleid: search-active-debug
+ini_set("display_errors","on");
+//ok: search-active-debug
+ini_set("display_errors","off");
+//ok: search-active-debug
+ini_set("display_errors",0);
+//ruleid: search-active-debug
+define("WP_DEBUG",true);
